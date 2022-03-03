15190867a674deba6b0d2581b4e42a03ce8b72f4
Etrix06 <alc18005@byui.edu>
2022-03-02 19:11:09 -0700

    added chalk border

--- testing.py
+++ testing.py
@@ -10,6 +10,7 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = 'Alphabet Snake'
 TILE_SCALING = 0.5
+SPRITE_SCALING_BOX = 0.5
 
 """Ryan 2/24/2022 - Change from arcade.Window to arcade.View and TestGame to TestView"""
 class TestView(arcade.View):
@@ -37,12 +38,26 @@ class TestView(arcade.View):
 
     # sets up the game variables
     def setup(self):
-        self.snake = snake.Snake(100, 300, 5)
+        self.snake = snake.Snake(105, 295, 5)
         self.goodfood = food.GoodLetterList()
         self.badfood = food.BadLetterList()
         self.background = arcade.load_texture("blackboard.jpg")            #Erik testing blackboard.jpg
         self.setup_letters('a') #NEEDS UPDATED TO TAKE A PARAMETER THAT IS THE NEXT LETTER IN THE WORD
-        #self.wall = 
+        self.wall = arcade.SpriteList()
+
+        # Manually create and position a box at 300, 200
+        #wall = arcade.Sprite("Alphabet\j.png", SPRITE_SCALING_BOX)
+        #wall.center_x = 300
+        #wall.center_y = 200
+        #self.wall.append(wall)
+
+        for x in range(95, 1200, 5):
+            wall = arcade.Sprite("Alphabet\chalk.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 285
+            self.wall.append(wall)
+
+
     
 
     # setup new lists of good and bad letters
@@ -68,7 +83,7 @@ class TestView(arcade.View):
         self.snake.draw()
         self.goodfood.draw()
         self.badfood.draw()
-
+        self.wall.draw()
         arcade.draw_text(f'Score: {self.score}', 20, SCREEN_HEIGHT-20, arcade.csscolor.WHITE, 12, font_name='arial')
 
 
