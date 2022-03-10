ac62535bf17aa9edaf4598917932fabea733ebfd
bhoward13fifa <howardbrad33@gmail.com>
2022-03-10 13:30:28 -0700

    Adjusted DB and created functionality to select random words.

---
+++ database.py
@@ -0,0 +1,71 @@
+import sqlite3
+import random
+
+
+def create_database():
+    conn = sqlite3.connect('words.db')
+
+    mycursor = conn.cursor()
+
+    mycursor.execute("DROP TABLE IF EXISTS easy_words")
+    mycursor.execute("DROP TABLE IF EXISTS normal_words")
+    mycursor.execute("DROP TABLE IF EXISTS hard_words")
+    mycursor.execute("DROP TABLE IF EXISTS game_words")
+
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS game_words (
+                    Id INTEGER NOT NULL PRIMARY KEY,
+                    game_word TEXT NOT NULL
+                    )""")
+
+    insert_words = [
+        (1, 'Airplane'),
+        (2, 'Basketball'),
+        (3, 'Cat'),
+        (4, 'Clock'),
+        (5, 'Hat'),
+        (6, 'House'),
+    ]
+    mycursor.executemany("INSERT INTO game_words VALUES (?,?)", insert_words)
+
+    conn.commit()
+
+    conn.close()
+
+
+def get_words():
+    conn = sqlite3.connect('words.db')
+
+    mycursor = conn.cursor()
+
+    mycursor.execute("SELECT * FROM game_words")
+
+    words_list = mycursor.fetchall()
+
+    return words_list
+
+
+def select_word():
+    game_words_list = get_words()
+
+    word_id = random.randrange(1, len(game_words_list)+1)
+
+    game_words = dict(game_words_list)
+
+    selected_word = game_words[word_id]
+
+    return selected_word
+
+
+def word_array():
+
+    word = select_word()
+
+    wordArray = [x for x in word]
+
+    print(wordArray)
+
+
+create_database()
+get_words()
+select_word()
+word_array()
