c478cc85e03eafa928e15d2f9149c5bfb6074be4
Ryan Taniguchi <83619555+ryantaniguchi@users.noreply.github.com>
2022-03-21 07:19:59 -0600

    Added comments

--- views.py
+++ views.py
@@ -2,17 +2,19 @@ import arcade
 from database import *
 import main
 
+# Screen settings
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 800
 
 # Class for the starting view that will show once a user loads the game
 class StartView(arcade.View):
 
+    # Creates the resources that will be used in the StartView
     def on_show(self):
-        # Creates the resources that will be used in the StartView
         self.background = arcade.load_texture("images/startview.jpg")
         self.yum = arcade.load_sound("sounds/yum.mp3")
 
+    # Creates and renders the screen
     def on_draw(self):
         self.clear()
         # Creates the background
@@ -35,17 +37,18 @@ class StartView(arcade.View):
 # Commands while in the InstructionView
 class InstructionView(arcade.View):
 
+    # Sets values for the Instruction background and for sound effects
     def on_show(self):
-        # Sets the background color of the StartView
         self.background = arcade.load_texture("images/instruction_screen.gif")
         self.yum = arcade.load_sound("sounds/yum.mp3")
 
+    # Clears what was previously on the screen and fills the screen with the selected background
     def on_draw(self):
         self.clear()
-        # Draws text on the screen
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,self.background)
 
+    # Allows users to move back to the StartView or begin gameplay
     def on_key_press(self, symbol, modifiers):
         if (symbol == arcade.key.LEFT):
             start_view = StartView()
@@ -56,7 +59,6 @@ class InstructionView(arcade.View):
             test_view.setup()
             self.window.show_view(test_view)    
 
-"""Ryan 3/10/2022 - added music for GameOverView"""
 # Screen and music that starts once a player dies
 class GameOverView(arcade.View):
     def __init__(self):
@@ -66,6 +68,7 @@ class GameOverView(arcade.View):
         # Background image that populates once the player dies
         self.texture = arcade.load_texture("images/game_over.jpg")
 
+    # Clears the screen of previous content and populates the screen with the background image
     def on_draw(self):
         self.clear()
         # Populates the screen with the background image
